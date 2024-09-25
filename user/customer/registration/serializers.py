f3a6569bd21b4b9c66fe0f7d7c81ace6abcc2bf1
parham <parham.khoshbin@digikala.com>
2024-09-25 11:14:30 +0330

    feat: add customer registration serializer

--- user/customer/registration/serializers.py
+++ user/customer/registration/serializers.py
@@ -1,7 +1,32 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 from ..models import Customer
+import re
 
 class CustomerSignUpSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True, validators=[
+        UniqueValidator(queryset=Customer.objects.all(), message="Email already exists")
+    ])
+
+    postal_code = serializers.CharField(required=True, min_length=10, max_length=10, error_messages={
+        'min_length': 'Postal code must be exactly 10 digits',
+        'max_length': 'Postal code must be exactly 10 digits'
+    })
+
+    password = serializers.CharField(write_only=True, min_length=8, error_messages={
+        'min_length': 'Password must be at least 8 characters long'
+    })
+
+    phone_number = serializers.CharField(
+        required=True,
+        min_length=11,
+        max_length=11,
+        error_messages={
+            'min_length': 'Phone number must be exactly 11 digits',
+            'max_length': 'Phone number must be exactly 11 digits'
+        }
+    )
+
     class Meta:
         model = Customer
         fields = [
@@ -18,12 +43,25 @@ class CustomerSignUpSerializer(serializers.ModelSerializer):
             'password'
         ]
 
-        extra_kwargs = {'password': {'write_only': True}}
+    def validate_email(self, value):
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", value):
+            raise serializers.ValidationError("Please enter a valid email address")
+        return value
+
+    def validate_postal_code(self, value):
+        if not value.isdigit():
+            raise serializers.ValidationError("Postal code must contain only digits")
+        return value
+
+    def validate_phone_number(self, value):
+        if not re.match(r"^09\d{9}$", value):
+            raise serializers.ValidationError("Phone number must be exactly 11 digits and start with '09'")
+        return value
 
     def create(self, validated_data):
         customer = Customer(
             email=validated_data['email'],
-            name=validated_data['first_name'],
+            name=validated_data['name'],
             last_name=validated_data['last_name'],
             national_id=validated_data['national_id'],
             phone_number=validated_data['phone_number'],
@@ -33,8 +71,6 @@ class CustomerSignUpSerializer(serializers.ModelSerializer):
             gender=validated_data['gender'],
             marital_status=validated_data['marital_status'],
         )
-
         customer.set_password(validated_data['password'])
         customer.save()
-
         return customer
