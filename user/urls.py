f28acf44d783537abd2667a8e35cb6fb53005dff
parham <parham.khoshbin@digikala.com>
2024-09-28 12:03:53 +0330

    feat: complete customer login

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from user.customer.authentication.views import CustomerLoginView
 from user.customer.registration.views import CustomerSignUpView
 
 urlpatterns = [
     path('customer/register/', CustomerSignUpView.as_view(), name='customer_signup'),
+    path('customer/auth/', CustomerLoginView.as_view(), name='customer_login'),
 ]
