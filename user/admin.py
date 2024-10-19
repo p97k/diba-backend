89862cd07d50ce69cc9a8270fe21bfbbc5fecb8c
parham <parham.khoshbin@digikala.com>
2024-10-19 10:35:25 +0330

    feat: add base Consultant admin

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,4 @@
+from .consultant.models import Consultant
 from .customer.models import Customer
 from django.contrib import admin
 
@@ -5,3 +6,8 @@ from django.contrib import admin
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ('id', 'email', 'first_name', 'last_name', 'national_id', 'phone_number', 'is_active')
     search_fields = ('email', 'first_name', 'last_name', 'phone_number', 'national_id')
+
+@admin.register(Consultant)
+class ConsultantAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'first_name', 'last_name', 'phone_number', 'service', 'is_active')
+    search_fields = ('email', 'first_name', 'last_name', 'phone_number', 'service', 'national_id')
--- user/consultant/models.py
+++ user/consultant/models.py
@@ -5,13 +5,15 @@ from django.utils import timezone
 class Consultant(models.Model):
     SERVICE_TYPES = [
         ('children', 'کودک و نوجوان'),
+        ('personal', 'مشاوره فردی'),
+        ('obstetrics', 'زنان و زایمان'),
     ]
 
     email = models.EmailField(unique=True, blank=False, null=False)
     first_name = models.CharField(max_length=255, blank=False)
     last_name = models.CharField(max_length=255, blank=False)
-    national_id = models.CharField(max_length=10, blank=False, default="")
-    phone_number = models.CharField(max_length=255, blank=False, default="")
+    national_id = models.CharField(max_length=10, blank=False)
+    phone_number = models.CharField(max_length=255, blank=False)
     gender = models.IntegerField(default=0)
     service = models.CharField(max_length=50, choices=SERVICE_TYPES)
     password = models.CharField(max_length=255, blank=False)
